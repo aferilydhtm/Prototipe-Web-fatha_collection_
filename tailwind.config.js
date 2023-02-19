2ca8b19a0b1716c8f5f8d7cf726b019b1708223d
aferilydtm <aferilyudhatama04@gmail.com>
2023-02-19 16:55:30 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        // 'sans': ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+        poppins: "'Poppins', sans-serif" 
+      }
+    },
+  },
+  plugins: [],
+}
